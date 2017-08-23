c1d5e6b7aebc520f2d986ec0a79b2481dcee2799
Luke <luke@lukecritchley.com>
2017-08-23 13:17:24 +0100

    Finally fixed StarterContent issue, should no longer be reverting now

---
+++ BattleTanks/Source/BattleTanks/BattleTanksGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BattleTanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKS_API ABattleTanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
